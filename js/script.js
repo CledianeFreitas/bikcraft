af897cba2b6bfda9f7ffb29c5a073cf2105332bb
Clediane Freitas <clediane.dev@gmail.com>
2022-11-13 15:46:49 -0400

    paginas internas

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// ATIVAR LINK
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+links.forEach(ativarLink);
+
+
+
+// ATIVAR itens do ORÇAMENTO
+
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.Checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
